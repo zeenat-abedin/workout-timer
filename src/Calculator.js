5d2a6ea6dc5e9eb21c790cc767c8a6a0afeee0ef
zeenat <zeenu.23@gmail.com>
2024-02-20 23:35:09 +0530

    Remove unnecessary dependencies

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,4 +1,4 @@
-import { memo, useCallback, useEffect, useState } from "react";
+import { memo, useEffect, useState } from "react";
 import clickSound from "./ClickSound.m4a";
 
 function Calculator({ workouts, allowSound }) {
